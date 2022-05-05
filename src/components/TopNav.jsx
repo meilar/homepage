cfa1f5a1146601de96a0d87c8258f9cf539bcc1e
Matt Eilar <meilar@gmail.com>
2022-05-05 16:14:02 -0700

    Update nav links for hash routing

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -10,10 +10,10 @@ const TopNav = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="homepage">
           <Nav className='me-auto'>
-            <Nav.Link href="/bio">Bio</Nav.Link>
+            <Nav.Link as={Link} to="/bio">Bio</Nav.Link>
             <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
-            <Nav.Link href="/projects">Projects</Nav.Link>
-            <Nav.Link href="/other">Other Work</Nav.Link>
+            <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+            <Nav.Link as={Link} to="/other">Other Work</Nav.Link>
             <NavDropdown title="Contact" id="basic-nav-dropdown">
               <NavDropdown.Item href="mailto:meilar@gmail?subject=Website%20Visitor">Email</NavDropdown.Item>
               <NavDropdown.Item href="https://www.github.com/meilar/">Github</NavDropdown.Item>
